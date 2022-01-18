0f7f873e3b774c7c775c819615676d18bc39166b
Ewan Harrow <ewan@harrow.org>
2022-01-18 20:39:05 +0000

    add binary search

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,11 @@ fn main() {
         None => (),
         Some(p) => println!("{:#?}", p),
     };
+
+    match binary_search(&data, 0, data.len() - 1, "01727"){
+        Some(n) => println!("Found {:#?}", data[n as usize].area),
+        None => ()
+    }
 }
 
 fn find_by_code<'a>(prefix: &str, values: &'a Vec<Place>) -> Option<&'a Place> {
@@ -41,6 +46,29 @@ fn starts_with_code<'a>(number: &str, values: &'a Vec<Place>) -> Option<&'a Plac
             println!("match {}: {}", number, item.area);
             return Some(item);
         }
+    }
+    None
+}
+
+fn binary_search(arr: &Vec<Place>, left: usize, right: usize, x: &str) -> Option<usize> {
+    // dbg!(left, right, x);
+    if left <= right && right >= 1 {
+        let mid = left + (right - left) / 2;
+        // println!("mid:{} - arr[mid]:{:#?}", mid, arr[mid].code);
+
+        // If the element is present at the
+        // middle itself
+        if arr[mid].code == x {
+            return Some(mid);
+        }
+         // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if arr[mid].code > x.to_string() {
+            // dbg!("element is smaller than mid so must be in left");
+            return binary_search(arr, left, mid - 1, x);
+        }
+        // dbg!("element is larger than mid so must be in right");
+        return binary_search(arr, mid + 1, right, x)
     }
     None
 }
