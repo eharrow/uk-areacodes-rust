ab4500a0cddaf6b5bf068e3288682691b1ec8827
Ewan Harrow <ewan@harrow.org>
2023-05-10 17:18:57 +0100

    refactor: change the imported module path so that library code is prefixed with api:: to make it clearer.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,21 +1,18 @@
-use uk_areacodes::api::binary_search;
-use uk_areacodes::api::find_by_code;
-use uk_areacodes::api::Place;
-use uk_areacodes::api::starts_with_code;
+use uk_areacodes::api;
 use uk_areacodes::data_source::json::UK;
 
 fn main() {
-    let data: Vec<Place> = serde_json::from_str(&UK).expect("JSON was not well-formatted");
+    let data: Vec<api::Place> = serde_json::from_str(&UK).expect("JSON was not well-formatted");
 
-    if let Some(p) = find_by_code("01727", &data) {
+    if let Some(p) = api::find_by_code("01727", &data) {
         println!("area is {:#?}", p.area);
     }
 
-    if let Some(p) = starts_with_code("01328", &data) {
+    if let Some(p) = api::starts_with_code("01328", &data) {
         println!("{:#?}", p)
     }
 
-    if let Some(n) = binary_search(&data, 0, data.len() - 1, "01503") {
+    if let Some(n) = api::binary_search(&data, 0, data.len() - 1, "01503") {
         println!("Found place named {:#?}", data[n as usize].area)
     }
 }
