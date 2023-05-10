9b5b0d156e5e0e42b0409b6f1c27d660ab73ea65
Ewan Harrow <ewan@harrow.org>
2023-05-10 17:58:53 +0100

    refactor: follows rust idioms of integration tests and bare main.rs in src

--- tests/basic_integration_test.rs
+++ tests/basic_integration_test.rs
@@ -1,18 +1,20 @@
 use uk_areacodes::api;
 use uk_areacodes::data_source::json::UK;
 
-fn main() {
+#[test]
+fn it_returns_matches() {
     let data: Vec<api::Place> = serde_json::from_str(&UK).expect("JSON was not well-formatted");
 
     if let Some(p) = api::find_by_code("01727", &data) {
-        println!("area is {:#?}", p.area);
+        assert_eq!("St Albans", p.area)
     }
 
     if let Some(p) = api::starts_with_code("01328", &data) {
-        println!("{:#?}", p)
+        assert_eq!("Fakenham", p.area);
     }
 
     if let Some(n) = api::binary_search(&data, 0, data.len() - 1, "01503") {
-        println!("Found place named {:#?}", data[n as usize].area)
+        assert_eq!("Looe", data[n as usize].area);
+
     }
 }
