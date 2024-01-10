9f5ae14784d860eb9652c8e7a52968c8c32ec3b8
Ewan Harrow <134166+eharrow@users.noreply.github.com>
2024-01-10 18:02:01 +0000

    feat: add finder to use a hash

--- tests/basic_integration_test.rs
+++ tests/basic_integration_test.rs
@@ -1,10 +1,12 @@
+use std::collections::HashMap;
 use uk_areacodes::api;
-use uk_areacodes::data_source::json::UK;
+use uk_areacodes::data_source::json::{UK_AS_MAP, UK_AS_SEQ};
 
 //noinspection SpellCheckingInspection
 #[test]
 fn it_returns_matches() {
-    let data: Vec<api::Place> = serde_json::from_str(&UK).expect("JSON was not well-formatted");
+    let data: Vec<api::Place> =
+        serde_json::from_str(&UK_AS_SEQ).expect("JSON was not well-formatted");
 
     if let Some(p) = api::find_by_code("01727", &data) {
         assert_eq!("St Albans", p.area)
@@ -16,5 +18,15 @@ fn it_returns_matches() {
 
     if let Some(n) = api::binary_search(&data, 0, data.len() - 1, "01503") {
         assert_eq!("Looe", data[n].area);
+    }
+}
+
+#[test]
+fn it_returns_matches_with_map() {
+    let data: HashMap<String, api::Place> =
+        serde_json::from_str(&UK_AS_MAP).expect("JSON was not well-formatted");
+
+    if let Some(p) = api::find_by_code_with_map("01727", &data) {
+        assert_eq!("St Albans", p.area)
     }
 }
